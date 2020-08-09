4f9e8937d5caf1c23dddd6d14266600b3249129d
akbastx <akbastx@gmail.com>
2020-08-09 10:16:48 -0500

    thirsd change from local

--- src/package2/FirstClass.java
+++ src/package2/FirstClass.java
@@ -5,7 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("This is the first change from local master");
 		System.out.println("This is the second change from local master");
-		
+		System.out.println("This is the third change from local master");
 	}
 
 }
